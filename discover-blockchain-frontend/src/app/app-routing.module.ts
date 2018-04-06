2b08a25b9d86e8f8b29478265c74114a3525bc17
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-04-06 22:09:41 +0200

    Adding new components - signIn, signUp, forgotPassword, confirmSignUp part 2

---
+++ discover-blockchain-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppConfig} from './config/app.config';
+import {SignInComponent} from "./components/sign-in/sign-in.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
+import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
+import {ConfirmSignUpComponent} from "./components/confirm-sign-up/confirm-sign-up.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '/', component: SignInComponent},
+  {path: 'login', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'confirm-sign-up', component: ConfirmSignUpComponent},
+  // otherwise redirect to 404
+  {path: '**', redirectTo: '/' + AppConfig.routes.error404}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {
+}
--- discover-blockchain-frontend/src/app/app.module.ts
+++ discover-blockchain-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {APP_CONFIG, AppConfig} from './config/app.config';
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {SignInComponent} from './components/sign-in/sign-in.component';
 import {SignUpComponent} from './components/sign-up/sign-up.component';
 import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
@@ -16,7 +17,8 @@ import {ConfirmSignUpComponent} from './components/confirm-sign-up/confirm-sign-
     ConfirmSignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: APP_CONFIG, useValue: AppConfig}

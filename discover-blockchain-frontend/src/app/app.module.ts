f0cf28adfa3e545be9c14036cbb270f87a042d26
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-04-08 04:28:21 +0200

    Adding translation module

--- discover-blockchain-frontend/src/app/app.module.ts
+++ discover-blockchain-frontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import {NgModule} from '@angular/core';
 import {APP_CONFIG, AppConfig} from './config/app.config';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {SignInComponent} from './components/sign-in/sign-in.component';
 import {SignUpComponent} from './components/sign-up/sign-up.component';
 import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
@@ -11,6 +14,11 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -26,7 +34,15 @@ import {SidebarComponent} from './components/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     {provide: APP_CONFIG, useValue: AppConfig}
